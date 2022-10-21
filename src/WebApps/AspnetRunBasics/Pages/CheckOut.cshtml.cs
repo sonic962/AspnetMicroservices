006d7454d7baae2abf1ec1416fddd5b00f53fd30
sonic962 <sonic.962@gmail.com>
2022-10-21 03:00:49 +0300

    Adding Web App

---
+++ src/WebApps/AspnetRunBasics/Pages/CheckOut.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics
+{
+    public class CheckOutModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public CheckOutModel(IBasketService basketService,IOrderService orderService)
+        {
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        [BindProperty]
+        public BasketCheckoutModel Order { get; set; }
+
+        public BasketModel Cart { get; set; } = new BasketModel();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Cart = await _basketService.GetBasket("swn");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostCheckOutAsync()
+        {
+            var username = "swn";
+            Cart = await _basketService.GetBasket(username);
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Order.UserName = username;
+            Order.TotalPrice = Cart.TotalPrice;
+
+            await _basketService.CheckoutBasket(Order);
+
+            
+            return RedirectToPage("Confirmation", "OrderSubmitted");
+        }       
+    }
+}
